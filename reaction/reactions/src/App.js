9a5e349abe860a0f966f28a7555fc19ab95bb808
Yooni <yoonikj@gmail.com>
2020-11-18 01:12:26 -0500

    finished reactions

--- reaction/reactions/src/App.js
+++ reaction/reactions/src/App.js
@@ -1,22 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { start_time: 0, ran_once: false, counting: false, true_duration: 0, reaction_time: 0, color: 'green'};
+    this.process_click = this.process_click.bind(this);
+  }
+  handle_color = (c) => {
+    this.setState({color: c});
+  }
+  start_count() {
+    var duration = Math.random() * 5000 + 2000
+    this.setState({
+      start_time: window.performance.now(),
+      true_duration: duration,
+      counting: true,
+      color: "darkred"
+    })
+    setTimeout(() => this.handle_color("green"), duration);
+  }
+  end_count() {
+    if ((window.performance.now()-this.state.start_time) >= this.state.true_duration) {
+      this.setState((prev) => ({
+        ran_once: true,
+        counting: false,
+        reaction_time: window.performance.now() - (prev.start_time + prev.true_duration)
+      }))
+    }
+  }
+  process_click() {
+    if (this.state.counting) {
+      this.end_count();
+    }
+    else {
+      this.start_count();
+    }
+  }
+  render() {
+    let msg = "Hello World!";
+    if (this.state.counting) {
+      if (this.state.color == "green") {
+        msg = "Click!"
+      } else {
+        msg = "Wait for Green"
+      }
+    } else {
+      if (this.state.ran_once == true) {
+        msg = "Your reaction time is " + this.state.reaction_time + " ms."
+      } else {
+        msg = "Click me to begin!"
+      }
+    }
+    return (
+      <div className = "PanelContainer" onClick = {this.process_click} style={ { background: this.state.color} }>
+        <div className = "Panel">{msg}
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className =  "Header">How Fast is your Reaction Time?</h1>
+        <Panel />
+        <p>Click as soon as the red box turns green. Click anywhere in the box to start.</p>
       </header>
     </div>
   );
